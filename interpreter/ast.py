58fc44df62a6666f7f7527e8adaa7e2a9e7fd9dd
nobody_on_earth <reddeadarthurfan1889@gmail.com>
2026-02-26 20:09:28 -0600

    Implement AST structure and to_ast function

---
+++ interpreter/ast.py
@@ -0,0 +1,51 @@
+ast = {
+  "VariableDeclaration": {
+    "TYPE": None,
+    "VALUE": None,
+    "NAME": None,
+  },
+  "BinaryExpression": {
+    "LEFT": None,
+    "RIGHT": None,
+    "SYMBOL": None,
+  },
+  "FunctionDefinition": {
+    "NAME": None,
+    "LOGIC": None,
+  },
+  "FunctionCalls": {
+    "FUNCTION": {
+      "ARGUMENT": None,
+      "NAME": None
+    }
+  }
+}
+types = ["int", "str", "bool", "list"]
+ops = ["+", "-", "/", "*"]
+identifiers = ["def", "print", ast["FunctionDefinition"]["NAME"]]
+builtins = ["print", "scanf"]
+def to_ast(line):
+    line = line.split()
+    operator = line[0]
+    if operator in types:
+      ast["VariableDeclaration"]["VALUE"] = "".join(line[3:])
+      ast["VariableDeclaration"]["NAME"] = line[1]
+      ast["VariableDeclaration"]["TYPE"] = line[0]
+      print(ast["VariableDeclaration"])
+    elif len(line) == 3:
+      if line[1] in ops:
+        ast["BinaryExpression"]["LEFT"] = line[0]
+        ast["BinaryExpression"]["RIGHT"] = line[2]
+        ast["BinaryExpression"]["SYMBOL"] = line[1]
+        print(ast["BinaryExpression"])
+    elif operator in identifiers:
+      if operator == "def":
+        ast["FunctionDefinition"]["NAME"] = line[1]
+        ast["FunctionDefinition"]["LOGIC"] = line[2]
+        print(ast["FunctionDefinition"])
+      elif operator.lower() in builtins:
+        ast["FunctionCalls"]["FUNCTION"]["ARGUMENT"] = line[1]
+        ast["FunctionCalls"]["FUNCTION"]["NAME"] = line[0]
+        print(ast["FunctionCalls"])
+    else:
+      pass
